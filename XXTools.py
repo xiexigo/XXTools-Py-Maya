8525f75c13692d31e24c5ad273580351b0455f95
xiexi <xiexigo@gmail.com>
2016-09-24 02:27:13 +0800

    初次提交XXTools

---
+++ XXTools.py
@@ -0,0 +1,240 @@
+﻿# -*- coding: UTF-8 -*-
+
+import maya.cmds as cmd
+
+#查找源节点
+def searchSourceNodes(nodeName):
+	connections = cmd.listConnections(nodeName, s=True, d=False)
+	if connections == None or len(connections) == 0:
+		return []
+	nodeList = []
+	for node in connections:
+		if nodeList.count(node) == 0 :
+			nodeList.append(node)
+	curList = nodeList
+	for node in curList:
+		nextSearchNodes = searchSourceNodes(node)
+		nodeList.extend(nextSearchNodes)
+	return nodeList
+
+#寻找输入源中所有的file节点
+def searchFileNodes(nodeList):
+	nodeList = nodeList or []
+	fileNode = []
+	for node in nodeList:
+		fileNode.extend(cmd.ls(searchSourceNodes(node), type="file"))
+	return fileNode
+
+class TextureEntity(object):	
+	def __init__(self, nodeName):
+		super(TextureEntity, self).__init__()
+		if cmd.objectType(nodeName) == "file":
+			self.textureNode = nodeName
+	def getFileName(self):
+		return cmd.getAttr(self.textureNode + ".fileTextureName")
+	def renameByFileName(self):
+		filepath = self.getFileName()
+		strs = filepath.split("/")
+		fileFullname = strs[-1]
+		filename = fileFullname.split(".")[0]
+		cmd.rename( self.textureNode, "tex_"+filename)
+
+#获得物体的UVSets
+def getUVSets(obj):
+    return cmd.polyUVSet(obj, query=True, allUVSets=True )
+#获得物体的UVSets的数量
+def getUVSetsCount(obj):
+    return len(getUVSets(obj))
+#选中UVSets数量不符的物体
+def checkSelectedObjectUVSetCount(number):
+	number = number or 1
+	objs = []
+	sel = cmd.ls(selection=True)
+	for obj in sel:
+		count = getUVSetsCount(obj)
+		if count != number:
+			objs.append(obj)
+	if len(objs)>0:
+	    cmd.select(objs)
+	else:
+		cmd.select(cl=True)
+
+
+#################################################
+
+# two array add one by one
+def arrayPlus(a,b):
+	result = []
+	for i in xrange(min(len(a),len(b))):
+		result.append(a[i]+b[i])
+	return result
+
+# [] -> [[],[],[],[]]
+def array2Matrix44(m):
+	return [\
+		[m[0],	m[1],	m[2],	m[3]],\
+		[m[4],	m[5],	m[6],	m[7]],\
+		[m[8],	m[9],	m[10],	m[11]],\
+		[m[12],	m[13],	m[14],	m[15]]]
+
+# [[1,2],[3,4]] * [[1,2],[3,4]] = 
+def matrixMultiply(a,b):
+	c = []
+	row = len(a)
+	col = len(b[0])
+	m = len(b)
+	for i in xrange(row):
+		c.append([]);
+		for j in xrange(col):
+			r = 0
+			for k in xrange(m):
+				r += a[i][k]*b[k][j]
+			c[i].append(r)
+	return c
+
+# Get localToWorld transform matrix
+def getWorldXformMatrix(obj):
+	mt = array2Matrix44(cmd.getAttr(obj+".xformMatrix"))
+	parents = cmd.listRelatives(obj, parent = True, fullPath = True)
+	if parents == None:
+		# no parent.
+		return mt
+	else:
+		pmt = getWorldXformMatrix(parents[0])
+		return matrixMultiply(mt,pmt)
+		
+# Get world translate
+def getWorldTranslate(obj):
+	mt = getWorldXformMatrix(obj)
+	return [mt[3][0],mt[3][1],mt[3][2]]
+
+class Node:
+	def __init__(self, mesh, vtxCount = 0):
+		self.mesh_ = mesh
+		vtxs = cmd.filterExpand(mesh + ".vtx[*]", ex=True, sm=31)
+		vtxCount = vtxCount or 0
+		self.vtxStart_ = vtxCount
+		self.vtxEnd_ = vtxCount + len(vtxs) - 1
+		self.children_ = []
+
+	def getVtx(self):
+		return ".vtx["+ str(self.vtxStart_) + ":"+ str(self.vtxEnd_) +"]"
+
+	def getEndVtxIndex(self):
+		return self.vtxEnd_
+
+	def setJoint(self,joint):
+		self.joint_ = joint
+	def getJoint(self):
+		return self.joint_
+
+	def addChild(self, node):
+		self.children_.append(node)
+	def getChildren(self):
+		return self.children_
+
+class Transform:
+	def __init__(self):
+		self.t = []
+		self.r = []
+		self.s = []
+		
+
+def getWorldTransfrom(obj, t):
+	rp = list(cmd.getAttr(obj+".rp")[0])
+	rpt = list(cmd.getAttr(obj+".rpt")[0])
+	p = arrayPlus(rp,rpt)
+	# convert to([x,y,z,1])
+	p.append(1)
+	mt = getWorldXformMatrix(obj)
+	wp = matrixMultiply([p], mt)
+	t.t = [wp[0][0],wp[0][1],wp[0][2]]
+	t.r = list(cmd.getAttr(obj+".rotate")[0])
+	t.s = list(cmd.getAttr(obj+".scale")[0])
+
+
+# Create joint by object, recursive function for autoBind()
+def createJointByObj(obj, vtxStartIndex = 0):
+	t = Transform()
+	getWorldTransfrom(obj, t)
+	node = Node(obj, vtxStartIndex)
+	jointName = ""
+	if vtxStartIndex == 0:
+		jointName = "root"
+	else:
+		jointName = obj.split("|")[-1]
+	joint = cmd.joint(a=True, p=tuple(t.t), s=tuple(t.s), n=jointName)
+	cmd.rotate(t.r[0], t.r[1], t.r[2], joint)
+	# print(joint)
+	node.setJoint(joint)
+	vtxStartIndex = node.getEndVtxIndex()+1
+
+	children = cmd.listRelatives(obj, c=True, f=True, typ="transform")
+	# print("vtxStartIndex ", vtxStartIndex)
+	if children!=None :
+		for c in children:
+			cmd.select(joint)
+			nodeInfo = createJointByObj(c, vtxStartIndex)
+			subNode = nodeInfo[0]
+			node.addChild(subNode)
+			vtxStartIndex = nodeInfo[1]
+	return (node, vtxStartIndex)
+
+# recursive call autoWeight for node and all children
+def autoSkinWeight(mesh,skin,node):
+	vtx = mesh + node.getVtx()
+	joint = node.getJoint()
+	print(skin,vtx,joint)
+	cmd.skinPercent(skin, vtx, transformValue=[(joint, 1.0)])
+	for subNode in node.getChildren():
+		autoSkinWeight(mesh, skin, subNode)
+
+# autoBind
+def autoBind(name = None):
+	obj = cmd.ls(sl = True, l = True)[0]	
+	cmd.select(d=True)
+	nodeInfo = createJointByObj(obj)
+	node = nodeInfo[0]
+	joint = node.getJoint()
+	print("joint ready!")
+	uniteInfo = cmd.polyUnite(obj, ch=1, mergeUVSets=1)
+	outObj = uniteInfo[0]
+	print("combin objects complete! newObject: " + outObj)
+	cmd.delete(uniteInfo, ch=True)
+	skin = cmd.skinCluster(joint, outObj, mi=2)[0]
+	autoSkinWeight(outObj,skin,node)
+	if name :
+		outObj = cmd.ls(cmd.rename(outObj, name), l = True)[0]
+	return (outObj,skin,node)
+
+def MultiBind(name = None):
+	objs = cmd.ls(sl = True, l = True, type = "transform")
+	if len(objs)>0 :
+		joints = []
+		nodes = []
+		parent = cmd.listRelatives(objs[0], parent = True, fullPath = True)
+		locator = "|"+cmd.spaceLocator(n = "root")[0]
+		locator = cmd.parent(locator, parent)
+		vtxStartIndex = 0
+		for obj in objs:
+			t = Transform()
+			getWorldTransfrom(obj, t)
+			node = Node(obj, vtxStartIndex)
+			cmd.select(locator, r = True)
+			joint = cmd.joint(a=True, p=tuple(t.t), s=tuple(t.s), n=obj.split("|")[-1])
+			cmd.rotate(t.r[0], t.r[1], t.r[2], joint)
+			node.setJoint(joint)
+			vtxStartIndex = node.getEndVtxIndex()+1
+			joints.append(joint)
+			nodes.append(node)
+		uniteInfo = cmd.polyUnite(objs, ch=1, mergeUVSets=1)
+		outObj = uniteInfo[0]
+		print("combin objects complete! newObject: " + outObj)
+		cmd.delete(uniteInfo, ch=True)
+		skin = cmd.skinCluster(joints, outObj, mi=2)[0]
+		for node in nodes:			
+			autoSkinWeight(outObj,skin,node)
+		if name :
+			outObj = cmd.ls(cmd.rename(outObj, name), l = True)[0]
+		outObj = cmd.parent(outObj, parent)[0]
+		return (outObj,skin,joints)
---
+++ tankBind.py
@@ -0,0 +1,143 @@
+import XXTools as xxt
+import maya.cmds as cmd
+import math as math
+
+#坦克绑定(全骨骼版)
+def TankBind():
+	for obj in cmd.ls(sl=True):
+	    t = cmd.getAttr(obj+".translate")[0]
+	    cmd.setAttr(obj+".translate", 0,0,0)
+	    body = None
+	    for c in cmd.listRelatives(obj, c=True, f=True, typ="transform"):
+	        print(c)
+	        if c.lower().find("body") >= 0:
+	            body = c
+	            break
+	    if body != None:
+	        cmd.select(body)
+	        bindInfo = xxt.autoBind("Body")
+	        root = bindInfo[2].getJoint()
+	        for j in cmd.listRelatives(root, c=True, f=True, typ="transform"):
+	            if j.lower().find("wheel") >= 0:
+	                attr = j+".rx"
+	                cmd.setKeyframe(attr, v=0, t=0)
+	                cmd.setKeyframe(attr, v=360, t=60)
+	                cmd.keyTangent(attr, itt = "spline", ott = "spline")
+	        cmd.setAttr(bindInfo[0]+".inheritsTransform", False)
+	        cmd.parent(bindInfo[0],root,obj)
+	    else:
+	        print("can't find body")
+	    print(t)
+	    cmd.setAttr(obj+".translate", t[0],t[1],t[2])
+
+#轮子分组
+def GroupWheel():
+	wheels = cmd.ls(sl = True, l = True, type = "transform")
+	if len(wheels)<=0:
+		return
+	cmd.makeIdentity(apply=True, t=1, r=1, s=1, n=0, pn=1)
+	cmd.CenterPivot()
+	parent = cmd.listRelatives(wheels[0], parent = True, fullPath = True)[0]
+	center = 0
+	if parent:
+		#如果有父物体，中心为父物体中心
+		pt = xxt.Transform()
+		xxt.getWorldTransfrom(parent, pt)
+		center = pt.t[0]
+	l = []
+	r = []
+	print("一共%d个轮子"%len(wheels))
+	for w in wheels:
+		t = xxt.Transform()
+		xxt.getWorldTransfrom(w, t)
+		#以中心为界左右分组
+		if t.t[0]<center:
+			l.append([w,t])
+		else:
+			r.append([w,t])
+	l.sort(lambda a,b:cmp(a[1].t[2], b[1].t[2]), reverse=True)#按z从大到小排序
+	r.sort(lambda a,b:cmp(a[1].t[2], b[1].t[2]), reverse=True)#按z从大到小排序
+	count = min(len(l),len(r))
+	print("一共%d排轮子"%(count))
+	ws = []
+	for i in range(count):
+		w = cmd.polyUnite([l[i][0], r[i][0]], name="Wheel%d"%(i))[0]
+		cmd.delete(w, ch=True)
+		w = cmd.parent(w, parent)[0]
+		ws.append(w)
+	cmd.select(ws, r=True)
+	cmd.makeIdentity(apply=True, t=1, r=1, s=1, n=0, pn=1)
+	cmd.CenterPivot()
+
+#绑定轮子添加轮子动画
+def BindWheel():
+	bindInfo = xxt.MultiBind(name = "Wheel")
+	joints = bindInfo[2]
+	for j in joints:
+		attr = j+".rx"
+		cmd.setKeyframe(attr, v=0, t=0)
+		cmd.setKeyframe(attr, v=360, t=30)
+		cmd.keyTangent(attr, itt = "spline", ott = "spline")
+
+#获取物体包围盒
+def GetSize(obj):
+	count = len(cmd.filterExpand(obj+".vtx[*]", ex=True, fp=True , sm=31))
+	_min = list(cmd.getAttr(obj+".vrts[0]")[0])
+	_max = list(cmd.getAttr(obj+".vrts[0]")[0])
+	for i in range(count):		
+		pos = cmd.getAttr(obj+".vrts[%d]"%i)[0]
+		_min[0] = min(_min[0], pos[0])
+		_min[1] = min(_min[1], pos[1])
+		_min[2] = min(_min[2], pos[2])
+		_max[0] = max(_max[0], pos[0])
+		_max[1] = max(_max[1], pos[1])
+		_max[2] = max(_max[2], pos[2])
+	return (_max[0] - _min[0], _max[1] - _min[1], _max[2] - _min[2])
+
+#设置炮台,制作动画
+def SetCannon(angle = 10, distance = 0.2):
+	cannon = cmd.ls(sl=True, l = True, type = "transform")[0]
+	barrel1 = cmd.listRelatives(cannon, c=True, f=True, typ="transform")[0]
+	barrel2 = cmd.listRelatives(barrel1, c=True, f=True, typ="transform")[0]
+	size = GetSize(barrel2)
+	print(size)
+	cmd.setAttr(barrel1+".rotateX", -10)
+	parent = cmd.listRelatives(cannon, parent = True, fullPath = True)[0]
+	barrel2 = cmd.parent(barrel2, parent)[0]
+	cannonShape = cmd.listRelatives(cannon, s=True, f=True)[0]
+	barrel1Shape = cmd.listRelatives(barrel1, s=True, f=True)[0]
+	barrel2Shape = cmd.listRelatives(barrel2, s=True, f=True)[0]
+	newCanon = cmd.polyUnite([cannonShape, barrel1Shape], name="Cannon")[0]
+	cmd.delete(newCanon, ch=True)
+	newCanon = cmd.parent(newCanon, parent)[0]
+	barrel2 = cmd.parent(barrel2, newCanon)[0]
+	start = cmd.getAttr(barrel2+".t")[0]
+	print(start)
+	d = size[2]*distance
+	offset = (0.0, math.sin(math.radians(angle))*d, math.cos(math.radians(angle))*d)
+	print(offset)
+	end = (start[0]+offset[0], start[1]-offset[1], start[1]-offset[1])
+	print(end)
+	cmd.setKeyframe(barrel2, at="translateY", v=start[1], t=31)
+	cmd.setKeyframe(barrel2, at="translateZ", v=start[2], t=31)
+	cmd.setKeyframe(barrel2, at="translateY", v=end[1], t=33)
+	cmd.setKeyframe(barrel2, at="translateZ", v=end[2], t=33)
+	cmd.setKeyframe(barrel2, at="translateY", v=start[1], t=42)
+	cmd.setKeyframe(barrel2, at="translateZ", v=start[2], t=42)
+	cmd.keyTangent(attr, itt = "linear", ott = "linear")
+	barrel2 = cmd.rename(barrel2, "barrel1")
+
+#坦克工具窗口
+def ShowWindowTank():
+	if cmd.window("windowTank", ex=True):
+		cmd.deleteUI("windowTank")
+	window = cmd.window("windowTank", t="Tank Binding", s=False, rtf=True)
+	cmd.columnLayout( adjustableColumn=True )
+	cmd.button(label="全骨骼版坦克绑定（过期）", w=300, command=("TankBind()"))
+	cmd.button(label="左右轮合并", w=300, command=("GroupWheel()"))
+	cmd.button(label="绑定轮子", w=300, command=("BindWheel()"))
+	cmd.button(label="设置炮台", w=300, command=("SetCannon()"))
+	cmd.setParent('..')
+	cmd.showWindow(window)
+
+ShowWindowTank()
